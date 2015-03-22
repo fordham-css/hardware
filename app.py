ec944c6f09ce4ad46347fbf6eba6fc728dca5a7b
Ken Durkin <kdurkin6@fordham.edu>
2015-03-22 07:14:13 +0000

    dev setup

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config=True)
 app.config["DEBUG"] = True
-app.config.from_object('config')
+#app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
