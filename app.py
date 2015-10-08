53f32b7646feede233dc5469118413d70f23b73f
Ken Durkin <kdurkin6@fordham.edu>
2015-10-08 00:02:33 +0000

    fetch items from gsheets api, add template

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template,request,jsonify
+import urllib, json
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -9,7 +10,12 @@ def page_not_found(error):
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+	url = "https://sheetsu.com/apis/4b26cc3d"
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
+	data = data['result']
+
+	return render_template("index.html", data=data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
